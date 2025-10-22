e27d96fb89b413f9fa3da9e124d05b3e74b0fe9c
IFL & Co. Ltda <54957378+ifl-co@users.noreply.github.com>
2025-10-22 02:48:38 -0300

    Refactor Get-InformacoesSistema for better clarity

--- Get-InformacoesSistema.ps1
+++ Get-InformacoesSistema.ps1
@@ -4,11 +4,11 @@ function Get-StringHash {
         [Parameter(Mandatory, ValueFromPipeline)]
         [string]$InputString,
 
-        [ValidateSet('SHA256','SHA1','MD5','SHA384','SHA512')]
+        [ValidateSet('SHA256', 'SHA1', 'MD5', 'SHA384', 'SHA512')]
         [string]$Algorithm = 'SHA256'
     )
     process {
-        $bytes  = [System.Text.Encoding]::UTF8.GetBytes($InputString)
+        $bytes = [System.Text.Encoding]::UTF8.GetBytes($InputString)
         $stream = [System.IO.MemoryStream]::new($bytes)
         try {
             (Get-FileHash -InputStream $stream -Algorithm $Algorithm).Hash
@@ -23,45 +23,63 @@ function Get-InformacoesSistema {
     [CmdletBinding()]
     param(
         [switch]$ComoTexto,
-
-        [ValidateSet('SHA256','SHA1','MD5','SHA384','SHA512')]
-        [string]$AlgoritmoHash = 'SHA256'
+        [System.Globalization.CultureInfo]$Culture
     )
 
+    begin {
+        if (-not $PSBoundParameters.ContainsKey('Culture')) {
+            $Culture = [System.Globalization.CultureInfo]::InvariantCulture
+        }
+    }
+
     process {
-        # Buscar apenas o necessário
         $props = @(
-            'BiosSMBIOSBIOSVersion',  # versão de BIOS (ex.: 1.29.1)
-            'OsVersion',              # versão do SO (ex.: 10.0.26100)
-            'OsSerialNumber',         # serial do SO (será hasheado)
-            'OsUptime'                # uptime (TimeSpan)
+            'BiosManufacturer', 'BiosName',
+            'OsName', 'OsVersion', 'OsInstallDate', 'OsSerialNumber',
+            'OsEncryptionLevel', 'OsUptime',
+            'CsUserName', 'WindowsRegisteredOwner', 'OsNumberOfUsers'
         )
 
         $ci = Get-ComputerInfo -Property $props
 
-        $serialHash = if ($ci.OsSerialNumber) {
-            Get-StringHash -InputString $ci.OsSerialNumber -Algorithm $AlgoritmoHash
-        } else { $null }
+        $installDateFmt = if ($ci.OsInstallDate -is [datetime]) {
+            $ci.OsInstallDate.ToString('yyyy-MM-dd HH:mm:ss', $Culture)
+        }
+        else { [string]$ci.OsInstallDate }
 
         $uptimeFmt = if ($ci.OsUptime -is [TimeSpan]) {
             '{0:%d}d {0:hh}h {0:mm}m {0:ss}s' -f $ci.OsUptime
-        } else { [string]$ci.OsUptime }
+        }
+        else { [string]$ci.OsUptime }
 
         $obj = [pscustomobject]@{
-            BiosNome       = $ci.BiosSMBIOSBIOSVersion
-            VersaoSO       = $ci.OsVersion
-            SerialSO_Hash  = $serialHash
-            UptimeSO       = $uptimeFmt
+            BiosManufacturer       = $ci.BiosManufacturer
+            BiosName               = $ci.BiosName
+            OsName                 = $ci.OsName
+            OsVersion              = $ci.OsVersion
+            OsInstallDate          = $installDateFmt
+            OsSerialNumber         = $ci.OsSerialNumber
+            OsEncryptionLevel      = $ci.OsEncryptionLevel
+            OsUptime               = $uptimeFmt
+            CsUserName             = $ci.CsUserName
+            WindowsRegisteredOwner = $ci.WindowsRegisteredOwner
+            OsNumberOfUsers        = $ci.OsNumberOfUsers
         }
 
         if ($ComoTexto) {
-            $ordem = 'BiosNome','VersaoSO','SerialSO_Hash','UptimeSO'
-            $ordem = ($ordem | ForEach-Object { ('{0}'+"`t"+'{1}') -f $_, $obj.$_ }) -join [Environment]::NewLine
+            $ordem = @(
+                'BiosManufacturer', 'BiosName',
+                'OsName', 'OsVersion', 'OsInstallDate', 'OsSerialNumber',
+                'OsEncryptionLevel', 'OsUptime',
+                'CsUserName', 'WindowsRegisteredOwner', 'OsNumberOfUsers'
+            )
+            $ordem = ($ordem | ForEach-Object { ('{0}' + "`t" + '{1}') -f $_, $obj.$_ }) -join [Environment]::NewLine
             $ordem = $ordem | Write-Output | Set-Clipboard
-        } else {
+        }
+        else {
             $obj
         }
+        Write-Host "Mapeamento concluído!" -ForegroundColor Yellow -BackgroundColor Black
+        Write-host "O resultado pode ser colado (ctrl+v) no local de destino." -ForegroundColor Yellow -BackgroundColor Black
     }
-}
-# O resultado será incorporado no clipboard, basta dar Ctrl+V no local desejado após execução
-Get-InformacoesSistema -ComoTexto;
+} Get-InformacoesSistema -ComoTexto
